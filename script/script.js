bbbe1b8d1976ee4a18fc2a6fad26f1d974e5e1ee
siumahmed <130205459+siumahmed@users.noreply.github.com>
2024-04-20 21:37:40 +0600

    first commit

---
+++ script/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById("toggleButton");
+    const menu = document.getElementById("menu");
+
+    toggleButton.addEventListener("click", function() {
+      menu.classList.toggle("hidden");
+    });
+  });
